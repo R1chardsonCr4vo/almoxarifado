eb5bef37222dbcfa8f58c3f6b04aea07a42ed2b6
R1chardsonCr4vo <chard_1046@live.com>
2026-06-13 17:46:15 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,8 @@
-const CACHE = 'almoxarifado-v2';
-
-self.addEventListener('install', e => {
-  self.skipWaiting();
-});
-
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => caches.delete(k)))
-    )
-  );
+  e.waitUntil(caches.keys().then(k => Promise.all(k.map(c => caches.delete(c)))));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', e => {
-  // Sempre busca da rede primeiro, sem cache
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  e.respondWith(fetch(e.request));
 });
